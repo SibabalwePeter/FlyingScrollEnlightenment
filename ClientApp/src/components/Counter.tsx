da6dea94bb09b1ba0c98cfd7355f12dc813ad4c7
Sibabalwe <sibabalwepeter2848@gmail.com>
2020-12-10 09:09:24 -0800

    First Commit

--- ClientApp/src/components/Counter.tsx
+++ ClientApp/src/components/Counter.tsx
@@ -15,7 +15,7 @@ class Counter extends React.PureComponent<CounterProps> {
             <React.Fragment>
                 <h1>Counter</h1>
 
-                <p>This is a simple example of a React components.</p>
+                <p>This is a simple example of a React component.</p>
 
                 <p aria-live="polite">Current count: <strong>{this.props.count}</strong></p>
 
