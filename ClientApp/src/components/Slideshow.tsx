9fe7b834cd598a160739f377b7be64f667e6bd06
Sibabalwe <sibabalwepeter2848@gmail.com>
2021-01-08 11:32:44 -0800

    Slide images edits

--- ClientApp/src/components/Slideshow.tsx
+++ ClientApp/src/components/Slideshow.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import '../Styles/Test.css';
-import RealEstate from '../Pictures/RealEstate.jpeg';
-import Accounting from '../Pictures/Accounting.jpeg';
+import Transportation from '../Pictures/Transportation.jpeg';
+import Farming from '../Pictures/Farming.jpeg';
 import Science from '../Pictures/Science.jpeg';
 
 const Slideshow = () => {
@@ -11,19 +11,25 @@ const Slideshow = () => {
                 {
                     `.item {
                         max-width: 100%;
-                        height: 250px;
+                        height: 300px;
+                    }
+                    img {
+                        display: block;
+                        margin-left: auto;
+                        margin-right: auto;
+                        opacity: 0.9;
+                        width: 100%;
                     }`
                 }
             </style>
             <div className="container">
-                <h2>Carousel Example</h2>
                 <div id="myCarousel" className="carousel slide" data-ride="carousel">
                     <ol className="carousel-indicators">
                         <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
                         <li data-target="#myCarousel" data-slide-to="1"></li>
                         <li data-target="#myCarousel" data-slide-to="2"></li>
                     </ol>
-                    <div className="carousel-inner">
+                    <div className="carousel-inner centre">
                         <div className="item active">
                             <img src={Science} alt="Los Angeles" />
                             <div className="carousel-caption">
@@ -32,14 +38,14 @@ const Slideshow = () => {
                             </div>
                         </div>
                         <div className="item">
-                            <img src={Accounting} alt="Chicago" />
+                            <img src={Farming} alt="Chicago" />
                             <div className="carousel-caption">
                                 <p>Its holistic model system designed to break the cycle of
                                     poverty and generational repetition of low educational attainment.</p>
                             </div>
                         </div>
                         <div className="item">
-                            <img src={RealEstate} alt="New York" />
+                            <img src={Transportation} alt="New York" />
                             <div className="carousel-caption">
                                 <p>We love the Big Apple!</p>
                             </div>
